b9e4e137fa9c1e6d54cdac920d769e41a495b401
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-05-31 18:54:00 +0400

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:5173',
       'http://localhost:4173',
-      'https://thechatter-423dc45d8040.herokuapp.com',
+      'https://chatter-eight-psi.vercel.app',
     ],
     credentials: true,
   });
